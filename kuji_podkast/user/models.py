7314d1379be5891e1c9bb164076b017a8c6b5d77
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-04 21:54:14 +0300

    add custom user model

---
+++ kuji_podkast/user/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'

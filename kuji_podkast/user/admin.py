f86c8fd168271c1e3c4b16446eda56bfde2c396e
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-04 21:56:20 +0300

    add admins

--- kuji_podkast/user/admin.py
+++ kuji_podkast/user/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('username', 'first_name', 'last_name', 'email', 'date_joined', 'is_staff', 'is_active')
+    list_display_links = ('username',)
+    list_editable = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)

36251ba8a0dcd41dcfeaf4849860a976cf327845
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-04 21:57:11 +0300

    add AUTH_USER_MODEL

--- kuji_podkast/kuji_podkast/settings.py
+++ kuji_podkast/kuji_podkast/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -99,11 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 

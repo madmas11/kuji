e80c39a3ab0e07a71a4458b54d64a6512f5e62e9
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-06 14:54:04 +0300

    add admins for models

--- kuji_podkast/video/admin.py
+++ kuji_podkast/video/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import VideoYoutube, Channel
 
-# Register your models here.
+
+class VideoYoutubeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'video_id', 'description', 'published_at', 'parts', 'channel')
+    search_fields = ('title', 'parts')
+    list_filter = ('parts',)
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'channel_id')
+
+
+admin.site.register(VideoYoutube, VideoYoutubeAdmin)
+admin.site.register(Channel, ChannelAdmin)

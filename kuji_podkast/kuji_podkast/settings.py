f3b2e33a40479d163d1e0555ca5e61d4ded75104
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-06 14:49:01 +0300

    add video app

--- kuji_podkast/kuji_podkast/settings.py
+++ kuji_podkast/kuji_podkast/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'video',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'video/static',
+    # '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

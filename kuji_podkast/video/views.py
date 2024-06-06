6e871882860124e460f49870884a62045959f57c
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-06 14:53:08 +0300

    add views for home page and detail

--- kuji_podkast/video/views.py
+++ kuji_podkast/video/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import VideoYoutube
 
-# Create your views here.
+
+def index(request):
+    video_for_index = VideoYoutube.objects.order_by('-published_at')[:5]
+    data = {
+        'title': 'Kuji-подкаст на связи',
+        'video_main': video_for_index
+    }
+    return render(request, 'index.html', data)
+
+
+def detail_video(request, pk):
+    video = VideoYoutube.objects.get(id=pk)
+    data = {
+        'video': video
+    }
+    return render(request, 'video/detail_video.html', data)

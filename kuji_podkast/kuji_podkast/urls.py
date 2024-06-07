cc0246b5ff75a337ca46e13751c776c70d8673fe
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-07 14:58:56 +0300

    add path for user app

--- kuji_podkast/kuji_podkast/urls.py
+++ kuji_podkast/kuji_podkast/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('video.urls')),
-    # path('user/', include('user.urls'))
+    path('user/', include('user.urls'))
 ]

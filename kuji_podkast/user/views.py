4d9a290c608bf2afc6f650637ef92ecf7d416245
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-07 14:58:06 +0300

    add views for a user page

--- kuji_podkast/user/views.py
+++ kuji_podkast/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import CustomUser
+from .forms import CustomUserChangeForm
 
-# Create your views here.
+
+def user_page(request, username):
+    user = get_object_or_404(CustomUser, username=username)
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_page', username=user.username)
+    else:
+        form = CustomUserChangeForm(instance=user)
+
+    title = f'Страница пользователя {user}'
+    data = {'user_name': user,
+            'title': title,
+            'form': form}
+    return render(request, 'user_page.html', data)

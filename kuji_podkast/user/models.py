793b9a9272d2ac1fd4f31c8d5962f095538d326a
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-09 16:49:04 +0300

    add favorite and comment models

--- kuji_podkast/user/models.py
+++ kuji_podkast/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from video.models import VideoYoutube
 
 
 class CustomUser(AbstractUser):
@@ -15,3 +16,29 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    video = models.ForeignKey(VideoYoutube, on_delete=models.CASCADE)
+    text = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:20]}"
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    video = models.ForeignKey(VideoYoutube, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.video.title}"
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'

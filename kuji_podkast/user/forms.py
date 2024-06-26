28ac590ec461edc8ac99541f29b8eb8ccb252616
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-26 18:35:22 +0300

    add form for comment

--- kuji_podkast/user/forms.py
+++ kuji_podkast/user/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +13,15 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
+        }
+        labels = {
+            'text': 'Введите комментарий'
+        }

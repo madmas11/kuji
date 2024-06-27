9f691378ddb9678388e9facc28498259f0bac1b9
Александр Макунин <alex-mak1108@yandex.ru>
2024-06-27 19:37:19 +0300

    added setigns for login/logout redirect

--- kuji_podkast/kuji_podkast/settings.py
+++ kuji_podkast/kuji_podkast/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

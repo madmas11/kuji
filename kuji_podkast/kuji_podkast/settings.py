9f65f6ecd589aa6303fc9a1ecd7e7bad52772e69
Александр Макунин <alex-mak1108@yandex.ru>
2024-07-04 16:23:33 +0300

    added setings for sending email

--- kuji_podkast/kuji_podkast/settings.py
+++ kuji_podkast/kuji_podkast/settings.py
@@ -133,3 +133,13 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'  # Ваш SMTP сервер
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@example.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
+
+DEFAULT_FROM_EMAIL = 'webmaster@lochalhost.com'
